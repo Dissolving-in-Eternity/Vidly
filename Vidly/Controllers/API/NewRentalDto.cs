45f72f51580c7d4a0af3edd46a9aa57208db238d
Prisoner Sense <prisonersense@yahoo.com>
2017-03-03 23:48:37 +0200

    Add Rental.

---
+++ Vidly/Controllers/API/NewRentalDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Vidly.Controllers.API
+{
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}
---
+++ Vidly/Controllers/API/NewRentalsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Vidly.Controllers.API
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
